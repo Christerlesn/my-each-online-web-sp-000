a53b96172022025e1ff0019bb5f7ded1e163050e
Christerle Nounoute <christerlesn@gmail.com>
2020-09-12 21:58:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,11 @@
+collection = [1, 2, 3, 4]
+
 def my_each(array)
-n = []
-  while n <= array.length
-    yield array[n]
+n = 0
+  while n < array.length
+    yield (array[n])
+    n = n + 1
   end
-  array
 end
 
-collection = [1, 2, 3, 4]
-my_each(collection) do |n|
-  n = collection
-#  print n
-end
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
